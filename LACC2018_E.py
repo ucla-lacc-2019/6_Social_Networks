cb526580a421569f97aa8e2c31b512a66942b16c
ucla-lacc-instructor <40631588+ucla-lacc-instructor@users.noreply.github.com>
2018-07-24 11:50:38 -0700

    Add files via upload

---
+++ LACC2018_E.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3.6
+
+# Graph Theory easy exercises for Social Networks module
+# LACC 2018
+
+#*************************************
+# This is where you write your code
+#
+# matrix_load()
+#
+# Loads an adjacency matrix for a graph from a file
+#
+# input: none
+# output: matrix containing each node 
+# 
+# Note: You can open a file using open(filename)
+# 
+# You can get a list containing each line with file.readlines()
+#
+#*************************************
+def matrix_load(filename):
+	
+#*************************************
+# This is where you write your code
+#
+# print_degrees(mat)
+#
+# Prints the degrees of all nodes in a graph given an adj. matrix 
+#
+# input: the adjacency matrix of the graph
+# output: none
+# 
+# Note: You don't need to return anything. 
+#
+# Effectively, you'll need to count the number of 1s in each row 
+# (or each column) and print this. Use a nested loop (for or while)
+#*************************************
+
+def print_degrees(mat):
+	
+#*************************************
+# This is where you write your code
+#
+# shortest_path(mat,node1,node2)
+#
+# Find the shortest path from node1 to node2 of all nodes in a graph given an adj. matrix 
+#
+# input: the adjacency matrix of the graph,node1,node2
+# output: 'The path is: ' appended with all edges on the path if there is one, otherwise print out 'No such path'
+# 
+# Note: You don't need to return anything. 
+#
+#*************************************
+
+def shortest_path(mat,node1,node2):
+	
+
+#*************************************
+# This is where you write your code
+#
+# detect_cycle(mat)
+#
+# Detect the in a graph given an adj. matrix 
+#
+# input: the adjacency matrix of the graph
+# output: 'Find a cycle, the first edge is: ' appended with an arbitrary edge on the cycle if there is one, otherwise print out 'No cycle in the graph'
+# 
+# Note: You don't need to return anything. 
+#
+#*************************************
+
+def detect_cycle(mat):
+	
